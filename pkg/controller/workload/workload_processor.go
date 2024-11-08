df7213c92daab0780bfc1afd8e39b01d955c742e
YaoZengzeng <yaozengzeng@huawei.com>
2024-11-08 08:17:19 +0000

    bugfix
    Signed-off-by: YaoZengzeng <yaozengzeng@huawei.com>

--- pkg/controller/workload/workload_processor.go
+++ pkg/controller/workload/workload_processor.go
@@ -767,6 +767,8 @@ func (p *Processor) handleAddressTypeResponse(rsp *service_discovery_v3.DeltaDis
 		}
 	}
 
+	p.handleServicesAndWorkloads(services, workloads)
+
 	p.handleRemovedAddresses(rsp.RemovedResources)
 	p.once.Do(p.handleRemovedAddressesDuringRestart)
 	return err
